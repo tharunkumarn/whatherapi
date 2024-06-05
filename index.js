f9ab3570632976a074eb15be99283ed95d074354
Tharun Kumar <tharun97kumar@gmail.com>
2024-06-05 23:47:06 +0530

    Implemented icons based on whether conditions

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use("/",async(req,res)=>{
    try {
      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=97ed86b99fdcf738c7a080e0fa9fde20&units=metric`);
      const result = response.data;
+     console.log(result);
+
      res.render("index.ejs", { result: result });
    } catch (error) {
      console.log(error.response.data);
@@ -26,24 +28,20 @@ app.use("/",async(req,res)=>{
 
 });
 // Handle form submission and fetch weather for the entered city
-app.post("/", async(req, res) => {
-   const city = req.body.city || "Anantapur";
-   console.log(city);
-   console.log("i Love you ❤️")
-   try {
-     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=97ed86b99fdcf738c7a080e0fa9fde20&units=metric`);
-     const result = response.data;
-    
-     console.log(result);
-     res.render("index", { result: results });
-   } catch (error) {
-     console.log(error.response.data);
-     res.status(500).send("Error fetching weather data");
-   }
- });
-// app.use("/",(req,res)=>{
-//    console.log("I love You ❤️ Da ")
-// });
+// app.post("/", async(req, res) => {
+//    const city = req.body.city || "Anantapur";
+  
+//    try {
+//      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=97ed86b99fdcf738c7a080e0fa9fde20&units=metric`);
+//      const result = response.data;
+   
+//      res.render("index", { result: results });
+//    } catch (error) {
+//      console.log(error.response.data);
+//      res.status(500).send("Error fetching weather data");
+//    }
+//  });
+
 app.listen(port,()=>{
    console.log(`The server port Number Running on ${port}`);
 });
